44303e06460829eed4efb3cd6c9267c6e5728966
saei1d <hehehe3bar@gmail.com>
2024-03-23 12:18:57 +0330

    bug fixed

--- main.py
+++ main.py
@@ -171,11 +171,14 @@ def handle_buy_callback(call):
 @bot.callback_query_handler(func=lambda call: call.data == "Ekhtesasi")
 def Ekhtesasi(call):
     bot.send_message(call.message.chat.id,
-                     " شمامیتوانید با وارد کردن تعداد روز مقدار حجم کانفیگ و تعداد کابر کانفیگ خودتون رو اختصاصی کنید \n\n  با وارد کردن عدد 0 اینگلیسی مقدار ثابت برای کانفیگ شما درنظر گرفته خواهد شد مثلا مقدار ثابت و حداقلی هر کانفیگ 40 روز و تعداد کاربر ۲ میباشد ",
+                     " شمامیتوانید با وارد کردن تعداد روز مقدار حجم کانفیگ و تعداد کابر کانفیگ خودتون رو اختصاصی کنید \n\n  با وارد کردن عدد 0 اینگلیسی مقدار ثابت برای کانفیگ شما درنظر گرفته خواهد شد مثلا مقدار ثابت و حداقلی هر کانفیگ 40 روز و تعداد کاربر 2 میباشد ",
                      reply_markup=ekhtesasiii())
-    if call.data == "make_config":
-        msg = bot.send_message(call.message.chat.id, "حجم مد نظر خودتون رو وارد کنید با عدد انگلیسی \n مثال:(150)")
-        bot.register_next_step_handler(msg, vol)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "make_config")
+def mmd(call):
+    msg = bot.send_message(call.message.chat.id, "حجم مد نظر خودتون رو وارد کنید با عدد انگلیسی \n مثال:(150)")
+    bot.register_next_step_handler(msg, vol)
 
 
 def vol(message):
@@ -191,22 +194,24 @@ def client(message):
     day = int(message.text)
     if day < 40:
         day = 40
-        bot.send_message(message.chat.id,"عددی که وارد کردید کوچکتر از 40 بود و برای شما همان 40 روز لحاظ شد")
-    client = bot.send_message(message.chat.id, "تعداد کاربران رو مشخص کنید و حداقل دوکاربر درنظر گرفته خواهد شد \n مثال:(3) ")
-    bot.register_next_step_handler(client,defa)
+        bot.send_message(message.chat.id, "عددی که وارد کردید کوچکتر از 40 بود و برای شما همان 40 روز لحاظ شد")
+    client = bot.send_message(message.chat.id,
+                              "تعداد کاربران رو مشخص کنید و حداقل دوکاربر درنظر گرفته خواهد شد \n مثال:(3) ")
+    bot.register_next_step_handler(client, defa)
+
+
 def defa(message):
     global clieee
-    clieee =int(message.text)
+    clieee = int(message.text)
     if clieee < 2:
         clieee = 2
-        bot.send_message(message.chat.id,"عددی که وارد کردید کوچکتر از 2 بود و برای شما همان 2 کاربر لحاظ شد")
+        bot.send_message(message.chat.id, "عددی که وارد کردید کوچکتر از 2 بود و برای شما همان 2 کاربر لحاظ شد")
     mmd = day - 40
     mmd2 = clieee - 2
-    su = (2400 * volume)+(1400 * mmd)+(mmd2*13000)
+    su = (2400 * volume) + (1400 * mmd) + (mmd2 * 13000)
 
-    bot.send_message(message.chat.id, f'کانفیگ شما با حجم {volume} و تعداد {day} روز و با تعداد کاربر {clieee} محاسبه شد \n \n    این کانفیگ با مبلغ {su}  تقدیم شما قرار خواهد گرفت')
-
-
+    bot.send_message(message.chat.id,
+                     f'کانفیگ شما با حجم {volume} و تعداد {day} روز و با تعداد کاربر {clieee} محاسبه شد \n \n    این کانفیگ با مبلغ {su}  تقدیم شما قرار خواهد گرفت')
 
 
 def fetch_trx_details(hash1, api_key, target_wallet_address):
