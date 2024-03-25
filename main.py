dc9a8e6c61ae3b46f7273bc5e8d57298c6b8b0d1
saei1d <hehehe3bar@gmail.com>
2024-03-25 15:27:52 +0330

    bug

--- main.py
+++ main.py
@@ -385,9 +385,10 @@ def disco(message, call):
             else:
                 print("b")
                 cur.execute("UPDATE users SET join_by_code = %s WHERE client_code = %s", (owner, client_code))
-                print(owner, client_code)
+                conn.commit()
                 bot.send_message(message.chat.id, f'کد تخفیف شما مورد تایید قرار گرفت به مقدار {discount_percentage}%')
                 handle_edame_kharid_callback(call, discount_percentage)
+                conn.comit()
         else:
             bot.send_message(message.chat.id, f'کد تخفیف شما مورد تایید قرار گرفت به مقدار {discount_percentage}%')
             handle_edame_kharid_callback(call, discount_percentage)
