93ffe1458db33393f7e23edc2493119e8bd8df31
saei1d <hehehe3bar@gmail.com>
2024-03-26 13:54:08 +0330

    bug

--- hiddify_api.py
+++ hiddify_api.py
@@ -55,15 +55,14 @@ def hiddify_api_get_conf(uuid):
 def show_configs(chat_id):
     url = "https://dub.barfarazabr.fun/awHquJhtnP/api/v2/admin/user/"
     secret_code = "a964952d-17d8-4e77-b1b5-0a42bdb0553c"
-    st = int(chat_id)
     response = requests.get(url, auth=(secret_code, ''))
-
+    print(type(chat_id))
     if response.status_code == 200:
         data = response.json()
         data_str = json.dumps(data)
         parsed_data = json.loads(data_str)
         for user in parsed_data:
-            if data['name'] == st:
+            if data['name'] == chat_id:
                 print("wdefef")
                 current_usages = user.get('current_usage_GB')
                 usage_limit = user.get('usage_limit_GB')
@@ -71,8 +70,7 @@ def show_configs(chat_id):
                 start_date = user.get('start_date')
                 uuid = user.get('uuid')
                 message = f"Current Usage: {current_usages} GB\nUsage Limit: {usage_limit} GB\nPackage Days: {pakages_date}\nStart Date: {start_date}\nUUID: {uuid}"
-
-                print(message)
-                return message
+            else:
+                print("dekedij")
     else:
         print("Error adding user", response.status_code, response.reason)
