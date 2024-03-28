50228b9ceda82b927809cb50a08553f9a2e65f29
saei1d <hehehe3bar@gmail.com>
2024-03-28 22:51:30 +0330

    buge

--- main.py
+++ main.py
@@ -411,8 +411,10 @@ def account_shakhsi2(message):
     tron_ekh = su / 7000
     tron_ekhh = round(tron_ekh, 2)  # گرد کردن به دو رقم اعشار
     print(tron_ekhh)
-    msg = bot.send_message(message.chat.id,
-                           "قبلا اضافه شده بود \n برای ساخت اکانت اختصاصی لازمه که به فرمت زیر ارسال کنی \n (30 40 2) \n (حجم روز کاربر) \n عدد اول حجم عدد دوم تعداد روز و عدد سوم تعداد کاربر را با یک اسپیس جدا کنید")
+    bot.send_message(message.chat.id,"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    bot.send_message(client_code_moshtari,
+                     f'حجم شما {num1} و تعداد روز شما {num2} و تعداد کاربر شما {num3} در نظر گرفتید  این کانفیگ به مبلغ {tron_ekhh} ترون به شما اراعه خواهد شد\n ',
+                     reply_markup=tarefe_ekhtesai_agent())
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "EEEE2")
